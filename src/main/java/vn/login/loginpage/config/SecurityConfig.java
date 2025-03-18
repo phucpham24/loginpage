f2ae1652bb46607931eb175246075b9476b77ab9
phucpham24 <phucphamhcmut@gmail.com>
2025-03-18 17:30:26 +0100

    add hash password

--- src/main/java/vn/login/loginpage/config/SecurityConfig.java
+++ src/main/java/vn/login/loginpage/config/SecurityConfig.java
@@ -6,11 +6,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = false)
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
--- src/main/java/vn/login/loginpage/controller/UserController.java
+++ src/main/java/vn/login/loginpage/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.login.loginpage.config.SecurityConfig;
 import vn.login.loginpage.domain.User;
 import vn.login.loginpage.service.UserService;
 import vn.login.loginpage.util.constant.GenderEnum;
@@ -14,6 +15,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +23,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
-    private final UserService userService;
+    private UserService userService;
 
-    public UserController(UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
@@ -32,6 +37,8 @@ public class UserController {
         if (this.userService.checkExistsByEmail(user.getEmail())) {
             throw new InvalidException("Email: " + user.getEmail() + " already exists");
         }
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
         User newUser = this.userService.handleSaveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
